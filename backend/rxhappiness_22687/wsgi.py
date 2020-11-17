bed283b2d05be1496bd711a64c2b9c02573c80a8
Crowdbotics <team@crowdbotics.com>
2020-11-17 01:53:08 +0000

    Update models

--- backend/rxhappiness_22687/wsgi.py
+++ backend/rxhappiness_22687/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rxhappiness_22687.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rxhappiness_22687.settings")
 
 application = get_wsgi_application()
